4b2049556e6b7e6f25d63c7907565749226d88db
Shiven <shiven.sharma5111@gmail.com>
2025-12-09 17:42:47 +0000

    frg

---
+++ problemsets.py
@@ -0,0 +1,18 @@
+import math
+import time
+import string
+import random
+
+# challenge 1
+
+work_time = int(input("How much did u work today? - "))
+
+sleep_time = 24 - work_time
+
+real_sleep_time = abs(sleep_time)
+ 
+print("This is how much u slept:", real_sleep_time)
+
+#challenge 2
+
+
