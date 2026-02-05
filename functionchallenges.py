b6b08c3f521befe64c93fc1bfcc79b0127f0b075
Shiven <shiven.sharma5111@gmail.com>
2026-02-05 01:26:40 +0000

    finally done

--- functionchallenges.py
+++ functionchallenges.py
@@ -25,46 +25,46 @@ multiply(13, 3)
 divide(27, 9)
 """
 
-def add(a, b):
-    print(a + b)
+# def add(a, b):
+#     print(a + b)
 
-def subtract(a, b):
-    print(a - b)
+# def subtract(a, b):
+#     print(a - b)
 
-def multiply(a, b):
-    print(a * b)
+# def multiply(a, b):
+#     print(a * b)
 
-def divide(a, b):
-    print(a / b)
+# def divide(a, b):
+#     print(a / b)
 
-add(34, 17)
-subtract(72, 24)
-multiply(13, 3)
-divide(27, 9)
+# add(34, 17)
+# subtract(72, 24)
+# multiply(13, 3)
+# divide(27, 9)
 
-# Challenge 2
-"""
-Psuedocode
-PROCEDURE average(a, b, c)
-{
-return (a + b + c)/3
-}
-num1 <-- input("Enter the first number: ")
-num2 <-- input("Enter the second number: ")
-num3 <--input("Enter the third number: ")
-result <-- average(num1, num2, num3)
-DISPLAY(result)
-"""
-def average(a, b, c):
+# # Challenge 2
+# """
+# Psuedocode
+# PROCEDURE average(a, b, c)
+# {
+# return (a + b + c)/3
+# }
+# num1 <-- input("Enter the first number: ")
+# num2 <-- input("Enter the second number: ")
+# num3 <--input("Enter the third number: ")
+# result <-- average(num1, num2, num3)
+# DISPLAY(result)
+# """
+# def average(a, b, c):
 
-    return(a + b + c) / 3
+#     return(a + b + c) / 3
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-num3 = int(input("Enter the third number: "))
-result = average(num1, num2, num3)
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# num3 = int(input("Enter the third number: "))
+# result = average(num1, num2, num3)
 
-print(result)
+# print(result)
 
 # challenge 3
 
@@ -72,11 +72,56 @@ print(result)
 Psuedocode
 PROCEDURE Is_Even(a)
 {
-IF(a%2 = 0)
-{
-DISPLAY(EVEN)
+    IF(a%2 = 0)
+    {
+    Return (That's even)
+    }
+    ELSE
+    {
+    Return (That's odd)
+    }
 }
-ELSE
-}
+num <-- INPUT(Is_Even(a))
+
+print(num)
 """
+
+# def Is_Even(a):
+
+#     if a%2 == 0:
+#         return "That's even"
+#     elif a%2 > 0:
+#         return "That's odd"
+
+    
+# num1 = int(input("Number?"))
+# result = Is_Even(num1)
+
+# print(result)
+
+# Challenge 3
+
+"""
+Psuedocode
+Already in the challenges as clue
+"""
+
+def analyze_word(word):
+
+    vowelCount = 0
+    consanentCount = 0
+
+    Vowels = "aeiou"
+
+    for i in range(len(word)):
+        if word[i] in Vowels:
+            vowelCount += 1
+        else:
+            consanentCount += 1
+    return f"Contains {vowelCount} vowels and {consanentCount} consanents"
+
+word1 = str(input("random word in all lowercases:"))
+result = f"here {analyze_word(word1)}"
+
+print(result)
 
